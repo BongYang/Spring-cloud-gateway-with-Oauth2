faa4f07105aa324462b0c10b529c63da7f1d77e0
BongYang <33821003+BongYang@users.noreply.github.com>
2022-02-22 21:09:08 +0900

    Make empty branch

--- README.md
+++
@@ -1,5 +0,0 @@
-# Spring-cloud-gateway-with-Oauth2
-
-1. Spring-cloud-gateway for routing
-2. OAuth2 for security
-3. WebFlux for reactive
--- src/main/java/com/example/springbootcloudgateway/user/repository/OAuth2UserInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootcloudgateway.user.repository;
-
-import com.example.springbootcloudgateway.user.domain.entity.OAuth2UserInfo;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
-
-public interface OAuth2UserInfoRepository extends ReactiveMongoRepository<OAuth2UserInfo, String> {
-    Mono<OAuth2UserInfo> findByEmail(String email);
-}
